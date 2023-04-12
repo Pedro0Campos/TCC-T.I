3726da8ef2dc81305c45cf5a8452ab7501228745
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-04-12 10:28:37 -0300

    Update database connection code

---
+++ docs/database/db.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $server = 'localhost';
+    $user = 'root';
+    $password = '';
+    $db_name = 'dbCoreo';
+
+    $conexao_db = mysqli_connect($server, $user, $password, $db_name);
+?>
---
+++ docs/database/dbCoreo.sql
@@ -0,0 +1,27 @@
+create database dbCoreo;
+
+use dbCoreo;
+
+create table `Usuarios` (
+	idUser int not null auto_increment primary key, 
+	nome varchar(50) not null,
+    email varchar(60) not null,
+    senha varchar(255) not null,
+    tipoUser int not null
+);
+
+create table `Ensaios` (
+	idEnsaio int not null auto_increment primary key,
+    idUser int not null,
+    Constraint FK_Ensaios_Usuarios foreign key (idUser) references Usuarios(idUser),
+    dataEnsaio date not null,
+    curtidas int,
+    horario time not null
+);
+
+create table `Comentarios` (
+	idComent int not null auto_increment primary key,
+    idUser int not null,
+    Constraint FK_Coment_User foreign key (idUser) references Usuarios(idUser),
+    txtComent varchar(300)
+);
