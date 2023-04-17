529c0bef82446f2dfea22c55019795d469bffbd4
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-04-17 17:58:14 -0300

    Update toggle

--- docs/navbar.php
+++ docs/navbar.php
@@ -21,7 +21,9 @@
     </ul>
 
     <ul class="navlista navlista-direita flexCenterVH">
-            <li class="duplo-li">Olá Fulano!<br><a href="#" class="navitens">Sair?</a></li>
-            <li><a href=""><img class="img-user" src="../docs/imagens/icon-user.png" alt=""></a></li>
-        </ul>
+        <li><a href="login.html" class="navitens">Login</a></li>
+        <li><a href="cadastro.php" class="navitens">Cadastra-se</a></li>
+        <!-- <li class="duplo-li">Olá Fulano!<br><a href="#" class="navitens">Sair?</a></li> -->
+        <!-- <li><a href=""><img class="img-user" src="../docs/imagens/icon-user.png" alt=""></a></li> -->
+    </ul>
 </div>
