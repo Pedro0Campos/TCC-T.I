5e7b3485a7ecfd19342ed5cbaddd30daab25608a
Etec <etec@etec.com>
2023-04-18 17:32:20 -0300

    Update toggler.js

--- docs/scripts/javascript/toggler.js
+++ docs/scripts/javascript/toggler.js
@@ -2,15 +2,20 @@
 function ocult_elements() {
     let checkbox = document.querySelector('#checkbox')
     let stars = document.getElementsByClassName('star')
+    let moon = document.getElementsByClassName('moon')
+    let sun = document.getElementsByClassName('sun')
     stars = Array.prototype.slice.call(stars)
 
     if (checkbox.checked) {
-        for (let i=0; i < stars.length; i++) {
-            stars[i].style.display = 'none'
-        }
+        stars[0].style.transform = 'translate(-220%, 50%) scale(0) rotate(180deg)'
+        stars[1].style.transform = 'translate(-100%, 280%) scale(0) rotate(180deg)'
+        stars[2].style.transform = 'translate(-220%, 280%) scale(0) rotate(180deg)'
+        moon[0].style.transform =     'rotate(210deg) translate(-5%, -10%) scale(0)'
+
     } else {
-        for (let i=0; i < stars.length; i++) {
-            stars[i].style.display = 'unset'
-        }
+        stars[0].style.transform = 'translate(-220%, 50%) scale(1) rotate(0deg)'
+        stars[1].style.transform = 'translate(-100%, 280%) scale(1) rotate(0deg)'
+        stars[2].style.transform = 'translate(-220%, 280%) scale(1) rotate(0deg)'
+        moon[0].style.transform =     'rotate(210deg) translate(-5%, -10%)'
     }
 }
