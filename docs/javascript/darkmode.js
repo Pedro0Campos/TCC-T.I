4d35be869376360f89fd5f4551d1cc1b0e0ef5d5
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-05-10 14:54:38 -0300

    Update navbar

---
+++ docs/javascript/darkmode.js
@@ -0,0 +1,26 @@
+function ocult_elements(checkbox) {
+    let itens = [document.querySelector('.sun'),
+                 document.querySelector('#cloud1'),
+                 document.querySelector('#cloud2'),
+                 document.querySelector('.moon'),
+                 document.querySelector('#star1'),
+                 document.querySelector('#star2'),
+                 document.querySelector('#star3')]
+
+    itens.forEach(element => {
+        element.classList.toggle('desativado')
+    })
+
+    // change_theme()
+}
+
+function change_theme() {
+    const sandy_danny = document.querySelector('.sandy-danny')
+    
+    html.classList.toggle('darkmode')
+    if (html.classList == 'darkmode') {
+        sandy_danny.src = 'imgs/home/sandy-danny-dark.png'
+    } else {
+        sandy_danny.src = 'imgs/home/sandy-danny-light.png'
+    }
+}
