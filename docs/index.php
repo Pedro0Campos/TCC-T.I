4cc064e64b222312abc2ec551e4d1acf09ecc88b
Etec <etec@etec.com>
2023-05-29 18:03:04 -0300

    Update

--- docs/index.php
+++ docs/index.php
@@ -40,11 +40,18 @@
     <!-- SplideJs -->
     <link rel="stylesheet" href="splide/css/splide.min.css">
     <script src="splide/js/splide.min.js"></script>
+
+    <!-- Spotify -->
+    <script src="https://open.spotify.com/embed-podcast/iframe-api/v1" async></script>
     
 </head>
 <body class="fade">
-    <div class="music-player ocult-player">
+    <div class="container-music-player ocult-player">
         <button onclick="musicPlayer()">Sair</button>
+        
+        <div class="music-player">
+            
+        </div>
     </div>
 
     <?php 
@@ -97,49 +104,49 @@
 
                         <div class="splide__track">
                             <ul class="splide__list">
-                                <div class="splide__slide" onclick="musicPlayer()">
+                                <div class="splide__slide track" onclick="musicPlayer()" data-spotify-id="spotify:track:2AVkArcfALVk2X8sfPRzya">
                                     <img src="../docs/imgs/home/musicas/Summer_Nights-Grease.png" alt="">
                                     <p class="nome_musica">Summer Nights</p>
                                     <p class="autor_musica">Grease</p>
                                 </div>
 
-                                <div class="splide__slide" onclick="musicPlayer()">
+                                <div class="splide__slide track" onclick="musicPlayer()" data-spotify-id="spotify:track:1XNE0QfNjdroSdosMIk8F6">
                                     <img src="../docs/imgs/home/musicas/Greased_Lightnin-Grease.png" alt="">
                                     <p class="nome_musica">Greased Lightnin'</p>
                                     <p class="autor_musica">Grease</p>
                                 </div>
 
-                                <div class="splide__slide" onclick="musicPlayer()">
+                                <div class="splide__slide track" onclick="musicPlayer()" data-spotify-id="spotify:track:0LfJkvPNCNEMLpZJgDQiV1">
                                     <img src="../docs/imgs/home/musicas/The_Wonder_of_You-Elvis Presley.png" alt="">
                                     <p class="nome_musica">The Wonder of You</p>
                                     <p class="autor_musica">Elvis Presley</p>
                                 </div>
 
-                                <div class="splide__slide" onclick="musicPlayer()">
+                                <div class="splide__slide track" onclick="musicPlayer()" data-spotify-id="spotify:track:1MUsWVRPLaC7ANKcEiMRCl">
                                     <img src="../docs/imgs/home/musicas/Dont_be_Cruel-Elvis_Presley.png" alt="">
                                     <p class="nome_musica">Don't be Cruel</p>
                                     <p class="autor_musica">Elvis Presley</p>
                                 </div>
 
-                                <div class="splide__slide" onclick="musicPlayer()">
+                                <div class="splide__slide track" onclick="musicPlayer()" data-spotify-id="spotify:track:1TKPfF2fvn6gVLVfp3iG4j">
                                     <img src="../docs/imgs/home/musicas/I_Walk_the_Line-Johnny Cash.png" alt="">
                                     <p class="nome_musica">I Walk The Line</p>
                                     <p class="autor_musica">Johnny Cash</p>
                                 </div>
                                 
-                                <div class="splide__slide" onclick="musicPlayer()">
+                                <div class="splide__slide track" onclick="musicPlayer()" data-spotify-id="spotify:track:1TrGdXSgiBm8W68D2K1COG">
                                     <img src="../docs/imgs/home/musicas/Cross_Road_Blues-Robert_Johnson.png" alt="">
                                     <p class="nome_musica">Cross Road Blues</p>
                                     <p class="autor_musica">Robert Johnson</p>
                                 </div>
 
-                                <div class="splide__slide" onclick="musicPlayer()">
+                                <div class="splide__slide track" onclick="musicPlayer()" data-spotify-id="spotify:track:0xpQWNukc7m4arb007HOH3">
                                     <img src="../docs/imgs/home/musicas/Backwater_Blues-Bill_Broonzy.png" alt="">
                                     <p class="nome_musica">Backwater Blues</p>
                                     <p class="autor_musica">Bill Broonzy</p>
                                 </div>
-                                <div class="splide__slide" onclick="musicPlayer()">
-                                    <img src="../docs/imgs/home/musicas/Sem_nome.png" alt="">
+                                <div class="splide__slide track" onclick="musicPlayer()" data-spotify-id="">
+                                    <img src="../docs/imgs/home/musicas/Tutti_Frutti-Little-Richard.png" alt="">
                                     <p class="nome_musica">Tutti Frutti</p>
                                     <p class="autor_musica">Little Richard</p>
                                 </div>
--- docs/javascript/spotify.js
+++ docs/javascript/spotify.js
@@ -1,8 +1,28 @@
 function musicPlayer() {
-    let player = document.querySelector('.music-player')
-
+    let player = document.querySelector('.container-music-player')
     player.classList.toggle('ocult-player')
+}
+
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    // Player
+    const music_player = document.querySelector('.music-player')
+
+    // Opções
+    const options = {
+        height: '100%',
+        width: '100%',
+    };
+
+    // itens que vão chamar o Music player
+    const callback = (EmbedController) => {
+        document.querySelectorAll('.track').forEach(
+            episode => {
+            episode.addEventListener('click', () => {
+                EmbedController.loadUri(episode.dataset.spotifyId)
+            });
+        })
+    };
     
-    // setInterval(() => {
-    // }, 1)
+    IFrameAPI.createController(music_player, options, callback);
+
 }
