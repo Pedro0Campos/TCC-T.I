0773e1320aafde9374a9782162199855dd049230
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-06-13 16:32:06 -0300

    Update login.php

--- docs/login.php
+++ docs/login.php
@@ -106,9 +106,9 @@
 
         ?>
 
-        <div class="left backg-image-logcad" data-aos="fade" data-aos-duration="500"></div>
+        <div class="form-left backg-image-logcad" data-aos="fade" data-aos-duration="500"></div>
 
-        <div class="right" data-aos="fade-down">
+        <div class="form-right" data-aos="fade-down">
 
             <!-- Retorna para o próprio link para criar a validação -->
             <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']). "?pagina=login"?>" method="post" autocomplete="off">
