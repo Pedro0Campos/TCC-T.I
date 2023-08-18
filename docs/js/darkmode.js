7904c1e8da68c67ea06d2ba8e6183ea6718a6452
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-08-18 20:09:21 -0300

    Update darkmode.js

--- docs/js/darkmode.js
+++ docs/js/darkmode.js
@@ -2,27 +2,9 @@ function darkmode() {
     let form = document.querySelector('#form-darkmode')
     let checkbox = document.querySelector('#checkbox')
     if (form && checkbox) {
-        checkbox.addEventListener('change', () => {
-            form.submit()
-        })
-        // form.submit()
+        checkbox.addEventListener('change', () => { form.submit() })
     }
-    // checkbox.addEventListener('change', () => {console.log('a');})
-
-    // let itens = [document.querySelector('.sun'),
-    //              document.querySelector('#cloud1'),
-    //              document.querySelector('#cloud2'),
-    //              document.querySelector('.moon'),
-    //              document.querySelector('#star1'),
-    //              document.querySelector('#star2'),
-    //              document.querySelector('#star3')]
-
-    // itens.forEach(element => {
-    //     element.classList.toggle('desativado')
-    // })
-
     const html = document.querySelector('html')
-    // html.classList.toggle('darkmode')
 
     const sandy_danny = document.querySelector('.sandy-danny')
     if (sandy_danny != null) {
