ed8a6ccc9a7ee3b703377328480d3ca1a2a45e1a
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-08-23 10:02:19 -0300

    Update scrolling.js

--- docs/js/scrolling.js
+++ docs/js/scrolling.js
@@ -1,7 +1,6 @@
 const SandyDanny = document.getElementById('SandyDanny')
 const SandyDannyClassList = SandyDanny.classList
 window.addEventListener('scroll', () => {
-    console.log(window.screen.height)
   if (window.scrollY >= window.screen.height - 500) {
     if (!SandyDannyClassList.contains('scrollHide')) {
         SandyDannyClassList.add('scrollHide')
