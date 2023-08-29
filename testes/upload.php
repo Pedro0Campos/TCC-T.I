a720f674b874b9dd8ea6ac35c0549bd69efdb289
Etec <henriquesc1442@gmail.com>
2023-08-29 14:39:44 -0300

    Update

--- testes/upload.php
+++ testes/upload.php
@@ -35,4 +35,17 @@
         echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
         $uploadOk = 0;
     }
+
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+      // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+
+    // https://www.w3schools.com/php/php_file_upload.asp
 ?>
